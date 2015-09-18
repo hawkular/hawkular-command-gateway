2851349d0bc615cb25f68351fa2624610715fdd2
Peter Palaga <ppalaga@redhat.com>
2015-09-18 20:27:56 +0200

    Do not log the whole message incl. authentication credentials

--- hawkular-command-gateway-war/src/main/java/org/hawkular/cmdgw/ws/server/UIClientCommWebSocket.java
+++ hawkular-command-gateway-war/src/main/java/org/hawkular/cmdgw/ws/server/UIClientCommWebSocket.java
@@ -98,8 +98,6 @@ public class UIClientCommWebSocket {
     public String uiClientMessage(String nameAndJsonStr, Session session) {
         String uiClientId = getUIClientIDFromSession(session);
         MsgLogger.LOG.infoReceivedMessageFromUIClient(uiClientId, session.getId());
-        MsgLogger.LOG.debugf("Received message from UI client [%s] (session [%s]): [%s]", uiClientId, session.getId(),
-                nameAndJsonStr);
 
         String requestClassName = "?";
         BasicMessage response;
